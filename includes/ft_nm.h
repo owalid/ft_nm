ca61b6f26f1168ade1ee1a50fdee4fa62d54caeb
owalid <othmane.el.ayadi@gmail.com>
2023-01-19 19:45:21 +0100

    feat: init

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -2,7 +2,12 @@
 #ifndef FTNM_H
 # define FTNM_H
 
-# include <fcntl.h>
-# include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <elf.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
+# include "ft_nm.h"
+# include "libft.h"
+
 
 int main(int argc, char *argv[]) {
-    return 0;
+    int fd;
+    char *ptr;
+    struct stat buf;
+
+    if (argc != 2) {
+        ft_putstr("usage: ./ft_nm [file]");
+        return (0);
+    }
+    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+        ft_putstr("error: open");
+        return (0);
+    }
+
+    if (fstat(fd, &buf) < 0) {
+        ft_putstr("error: fstat");
+        return (0);
+    }
+
+    if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+        ft_putstr("error: mmap");
+        return (0);
+    }
 }
     
